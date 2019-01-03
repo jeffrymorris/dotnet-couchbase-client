c3faa7c587c89200b3b209777b29efd9418f4a6d
jeffrymorris <jeffrymorris@gmail.com>
2019-01-02 17:35:20 -0800

    Add IResult and IGetResult

---
+++ src/Couchbase/IResult.cs
@@ -0,0 +1,15 @@
+﻿
+using Couchbase.Core.IO.Errors;
+using Couchbase.Core.IO.Operations;
+
+namespace Couchbase
+{
+    public interface IResult
+    {
+        ulong Cas { get; }
+	
+        ResponseStatus Status { get; }
+
+        ErrorCode Error(); //from enhanced error handling
+    }
+}
