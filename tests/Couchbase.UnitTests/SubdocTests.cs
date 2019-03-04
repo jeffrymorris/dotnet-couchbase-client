4066e070824cd6017bfc129c3bed66c6bac24246
Mike Goldsmith <goldsmith.mike@gmail.com>
2019-03-04 17:05:49 +0000

    move ClusterFixture into separate file and update references

--- tests/Couchbase.UnitTests/SubdocTests.cs
+++ tests/Couchbase.UnitTests/SubdocTests.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Threading.Tasks;
+using Couchbase.UnitTests.Fixtures;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -93,42 +93,6 @@ namespace Couchbase.UnitTests
                 name = "mike"
             };
             Assert.Equal(JsonConvert.SerializeObject(expected), content);
-        }
-    }
-
-    public class ClusterFixture : IDisposable
-    {
-        public ICluster Cluster { get; }
-
-        public ClusterFixture()
-        {
-            var cluster = new Cluster();
-            var task = cluster.Initialize(
-                new Configuration()
-                    .WithServers("couchbase://127.0.0.1")
-                    .WithBucket("default")
-                    .WithCredentials("Administrator", "password")
-            );
-            task.ConfigureAwait(false);
-            task.Wait();
-
-            Cluster = cluster;
-        }
-
-        public async Task<IBucket> GetDefaultBucket()
-        {
-            return await Cluster.Bucket("default");
-        }
-
-        public async Task<ICollection> GetDefaultCollection()
-        {
-            var bucket = await GetDefaultBucket();
-            return await bucket.DefaultCollection;
-        }
-
-        public void Dispose()
-        {
-            //Cluster?.Dispose();
         }
     }
 }
